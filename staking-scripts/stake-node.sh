d4da5d977158e54e5f21b0a1416325bc3f06f8a1
frankf1957 <49395034+frankf1957@users.noreply.github.com>
2025-02-03 23:04:08 -0500

    added calculation for gas limit

--- staking-scripts/stake-node.sh
+++ staking-scripts/stake-node.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 
-GAS_LIMIT=25000000
 PROXY="https://gateway.validators.onefinity.network"
 
 NODES=1
+
+# stake amount for each individual node - 2500 $ONE x the number of nodes
 VALUE=$(echo "2500 * $NODES * 10^18" | bc)
+
+# gas limit of 6,000,000 gas units × the number of nodes
+GAS_LIMIT=$(echo "6 * 10^6 * $NODES" | bc)
 
 
 mxpy validator stake \
